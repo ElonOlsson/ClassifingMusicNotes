9c03a3d34e3b2a85d43a57fe6677bf46321b4a7d
ElonOlsson <olssonelon@gmail.com>
2018-11-14 14:54:58 +0100

    rotation works

--- test1.m
+++ test1.m
@@ -2,11 +2,7 @@ function strout = test1(Im)
     strout = '1';
     bw= 1-imbinarize(Im(:,:,1), 0.7);   % 1- pga. objekt räknas som vitt på svart.
    
-  
-   
-    
     angle = findRotationAngle(bw)
-    
     if (angle > 0 || angle < 0)
         bw = imrotate(bw, angle);
         disp("rotated by angle: " + angle)
