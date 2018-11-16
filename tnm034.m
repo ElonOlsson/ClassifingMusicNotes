14d782c3439e868262d501f57a829c6e09c53cfe
ElonOlsson <olssonelon@gmail.com>
2018-11-16 11:50:39 +0100

    abs angle

--- tnm034.m
+++ tnm034.m
@@ -1,6 +1,7 @@
 function strout = tnm034(Im)
     strout = '1';
-    bw= 1-imbinarize(Im(:,:,1), 0.7);   % 1- pga. objekt räknas som vitt på svart.
+    binarizeThreshold = 0.8;
+    bw= 1-imbinarize(Im(:,:,1), binarizeThreshold);   % 1- pga. objekt räknas som vitt på svart.
 
     angle = findRotationAngle(bw)
     if (angle > 0 || angle < 0)
@@ -8,14 +9,14 @@ function strout = tnm034(Im)
         disp("rotated by angle: " + angle)
     end
     
-    bw= 1-imbinarize(Im(:,:,1), 0.7);
-    cropHeight = tan(deg2rad(angle))*size(Im,2);
-    cropWidth = tan(deg2rad(angle))*size(Im,1);
+    bw= 1-imbinarize(Im(:,:,1), binarizeThreshold);
+    % Crop rotation corner articacts 
+    cropHeight = tan(deg2rad(abs(angle)))*size(Im,2);
+    cropWidth = tan(deg2rad(abs(angle)))*size(Im,1);
     rect = [cropWidth, cropHeight, size(bw,2)-2*cropWidth, size(bw,1)-2*cropHeight];
     bw = imcrop(bw, rect);
     
-    
-    
+    % Show
     figure(1)
     plot(sum(bw, 2), 1:size(bw,1));
     figure(2)
